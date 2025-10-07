dfddf63bc0deacf06ed9631ccbf08cdb76883a19
Serranomanuel <Serranomanuel106@gmail.com>
2025-10-06 22:47:24 -0500

    Ejercicio 3

---
+++ Ejercicio3.js
@@ -0,0 +1,10 @@
+let num = parseInt(prompt(`Ingresa un numero entero: `))
+
+if (num == 0 ){
+    console.log(`CERO`);
+}else if (num > 0){
+    console.log(`Numero positivo`);
+}else{
+    console.log(`Numero negativo`);
+    
+}
